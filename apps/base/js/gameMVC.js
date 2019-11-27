db7a4e1dac164e176762551163484491a1277ebe
TheMagnat <guillaume.magniadas@gmail.com>
2019-11-27 17:47:50 +0100

    created a new Model view and controller for game screen.

---
+++ apps/base/js/gameMVC.js
@@ -0,0 +1,44 @@
+
+
+class gameModel extends Model {
+
+	constructor() {
+		super();
+	}
+
+	async initialize(mvc) {
+		super.initialize(mvc);
+
+	}
+
+}
+
+
+
+class gameView extends View {
+	constructor() {
+		super();
+	}
+
+	initialize(mvc) {
+		super.initialize(mvc);
+
+		this.stage.style.backgroundColor = "green";
+
+	}
+}
+
+
+class gameController extends Controller {
+
+	constructor() {
+		super();
+	}
+
+	initialize(mvc) {
+		super.initialize(mvc);
+
+	}
+
+
+}
